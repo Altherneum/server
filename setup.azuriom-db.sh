d7b8dd5d0daca342afcc914b519eca86c06b89f5
lx78WyY0J5 <9j2k37st@gmail.com>
2024-04-27 14:54:17 +0200

    update azuriom db config download

--- setup.azuriom-db.sh
+++ setup.azuriom-db.sh
@@ -1,3 +1,5 @@
 wget -O /setup.azuriom-settings.sql 'https://raw.githubusercontent.com/Altherneum/server/main/setup.azuriom-settings.sql'
+wget -O /setup.azuriom-images.sql 'https://raw.githubusercontent.com/Altherneum/server/main/setup.azuriom-images.sql'
+#
 # WIP, feature on test / active dev
 # mysql -h "server-name" -u "your_username" -p "your_password" "database-name" < "filename.sql"
