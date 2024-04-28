ba6e9ba43700b8d3963a22b544db5c25e1a07da3
lx78WyY0J5 <9j2k37st@gmail.com>
2024-04-28 16:02:54 +0200

    add mkdir for scripts / sh

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+mkdir -p /scripts/sh
 wget -O /scripts/sh/update.runner.sh 'https://raw.githubusercontent.com/Altherneum/server/main/scripts/sh/update.runner.sh'
 chmod +x /scripts/sh/update.runner.sh
 /scripts/sh/update.runner.sh
