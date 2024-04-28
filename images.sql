6b74c1549cbc6ff85acad5b7eba45bfe210559d1
lx78WyY0J5 <9j2k37st@gmail.com>
2024-04-28 15:26:49 +0200

    update image DB

---
+++ images.sql
@@ -0,0 +1,30 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `file` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `images_file_unique` (`file`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+REPLACE INTO `images` (`id`, `name`, `file`, `type`, `created_at`, `updated_at`) VALUES
+(1, 'background', 'background.jpg', 'image/jpeg', '2024-04-27 12:39:57', '2024-04-27 12:39:57'),
+(2, 'gif', 'gif.gif', 'image/gif', '2024-04-27 12:43:24', '2024-04-27 12:43:24'),
+(3, 'icone', 'icone.png', 'image/png', '2024-04-27 12:44:11', '2024-04-27 12:44:11'),
+(4, 'logogif', 'small.gif', 'image/gif', '2024-04-27 12:44:11', '2024-04-27 12:44:11');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- setup.azuriom-db.sh
+++ setup.azuriom-db.sh
@@ -7,3 +7,7 @@ mysql site < /social_links.sql
 wget -O '/navbar_elements.sql' 'https://raw.githubusercontent.com/Altherneum/server/main/navbar_elements.sql'
 mysql site < /navbar_elements.sql
 #
+wget -O '/images.sql' 'https://raw.githubusercontent.com/Altherneum/server/main/images.sql'
+mysql site < /images.sql
+#
+# Clean Azuriom cache after this to update website
