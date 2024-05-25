de77c3266bf855c2b18c4e87e875bd1524138cb8
lx78WyY0J5 <9j2k37st@gmail.com>
2024-05-25 19:25:19 +0200

    Update start.sh set max ram usage

--- DiscordBot/start.sh
+++ DiscordBot/start.sh
@@ -1,4 +1,4 @@
 cd /DiscordBot
 screen -XS bot quit
-screen -dmS bot java -jar /DiscordBot/bot.jar
+screen -dmS bot java -Xms16M -Xmx64M -XX:SoftMaxHeapSize=64M -jar /DiscordBot/bot.jar
 echo screen bot
