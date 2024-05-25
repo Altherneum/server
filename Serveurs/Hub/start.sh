92e9f02047c811a3404df81cc183fbbbae5c1f08
lx78WyY0J5 <9j2k37st@gmail.com>
2024-05-25 04:14:07 +0200

    lower ram usage for hub

--- Serveurs/Hub/start.sh
+++ Serveurs/Hub/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/Hub/
-screen -dmS hub java -Xms256M -Xmx1024M -XX:SoftMaxHeapSize=256M -Dterminal.jline=false -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Hub/paper.jar nogui
+screen -dmS hub java -Xms256M -Xmx512M -XX:SoftMaxHeapSize=256M -Dterminal.jline=false -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Hub/paper.jar nogui
 echo screen hub
