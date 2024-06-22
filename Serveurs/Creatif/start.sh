8e8404b49009283440f94bb90a34e1cd81ae7a0d
lx78WyY0J5 <9j2k37st@gmail.com>
2024-06-22 16:49:49 +0200

    removed a = that shouldn't be there

--- Serveurs/Creatif/start.sh
+++ Serveurs/Creatif/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/Creatif/
-screen -dmS creatif java -Xms128M -Xmx=512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Creatif/paper.jar nogui
+screen -dmS creatif java -Xms128M -Xmx512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Creatif/paper.jar nogui
 echo screen creatif
