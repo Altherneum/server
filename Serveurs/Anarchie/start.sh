d9e4aa9fef87c71823ef94d67865dbffd6a5d993
lx78WyY0J5 <9j2k37st@gmail.com>
2024-06-22 16:17:24 +0200

    update RAM usage

--- Serveurs/Anarchie/start.sh
+++ Serveurs/Anarchie/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/Anarchie/
-screen -dmS anarchie java -Xms512M -Xmx1536M -XX:SoftMaxHeapSize=256M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Anarchie/paper.jar nogui
+screen -dmS anarchie java -Xms64M -Xmx256M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Anarchie/paper.jar nogui
 echo screen anarchie
--- Serveurs/OPPrison/start.sh
+++ Serveurs/OPPrison/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/OPPrison/
-screen -dmS OPPrison java -Xms512M -Xmx1536M -XX:SoftMaxHeapSize=256M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/OPPrison/paper.jar nogui
+screen -dmS OPPrison java -Xms128M -Xmx=512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/OPPrison/paper.jar nogui
 echo screen OPPrison
