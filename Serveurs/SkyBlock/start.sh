5cacbd9fbc1a216160e93ab0bc87ffb05a12e4a5
lx78WyY0J5 <9j2k37st@gmail.com>
2024-08-09 22:16:16 +0200

    more RAM for servers

--- Serveurs/SkyBlock/start.sh
+++ Serveurs/SkyBlock/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/SkyBlock/
-screen -dmS SkyBlock java -Xms128M -Xmx512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/SkyBlock/paper.jar nogui
+screen -dmS SkyBlock java -Xms256M -Xmx512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/SkyBlock/paper.jar nogui
 echo screen SkyBlock
