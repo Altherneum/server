69435b5149f5cedd15af24f8aaf408165969ac08
lx78WyY0J5 <9j2k37st@gmail.com>
2024-08-10 16:38:25 +0200

    RAM update & start only survie

--- Serveurs/Survie/start.sh
+++ Serveurs/Survie/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/Survie/
-screen -dmS survie java -Xms256M -Xmx512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Survie/paper.jar nogui
+screen -dmS survie java -Xms256M -Xmx1024M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Survie/paper.jar nogui
 echo screen survie
