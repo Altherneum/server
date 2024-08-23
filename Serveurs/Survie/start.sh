c9badd81016d55b59cabe16c6edd6b162fe02443
lx78WyY0J5 <9j2k37st@gmail.com>
2024-08-23 19:46:41 +0200

    Slight more RAM for survival serv

--- Serveurs/Survie/start.sh
+++ Serveurs/Survie/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/Survie/
-screen -dmS survie java -Xms256M -Xmx512M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Survie/paper.jar nogui
+screen -dmS survie java -Xms256M -Xmx768M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Survie/paper.jar nogui
 echo screen survie
