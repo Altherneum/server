c2760292bf15ac849710de79510b44904b71b568
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-24 16:10:27 +0200

    added admin user to replace root login by default

--- scripts/sh/setup.system.user.sh
+++ scripts/sh/setup.system.user.sh
@@ -3,3 +3,8 @@ useradd -r -g minecraft -d "/Serveurs" -s "/bin/bash" minecraft
 
 groupadd -r discord
 useradd -r -g discord -d "/DiscordBot" -s "/bin/bash" discord
+
+password="SuperPassword123!"
+pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+groupadd -r admin
+useradd -g admin -d "/home/admin" -s "/bin/bash" --create-home -p "$pass" admin
