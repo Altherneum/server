1601f29cfa79994622b1ff678ca4e46bd8453f89
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-24 23:45:00 +0200

    added a rule to force permitemptypasswords to no

--- scripts/sh/setup.system.ssh.sh
+++ scripts/sh/setup.system.ssh.sh
@@ -3,5 +3,6 @@ touch /etc/ssh/sshd_config.d/hardening.conf
 echo "PermitRootLogin no" >> /etc/ssh/sshd_config.d/hardening.conf
 echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config.d/hardening.conf
 # echo "PasswordAuthentication no" >> /etc/ssh/sshd_config.d/hardening.conf # Créer une clef SSH avant d'activer
+echo "permitemptypasswords no" >> /etc/ssh/sshd_config.d/hardening.conf
 
 systemctl restart sshd
