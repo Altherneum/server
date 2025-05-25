caac041f560df1205b2230d67831269821a2fa44
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-25 07:20:08 +0200

    finish

--- scripts/sh/setup.system.motd.sh
+++ scripts/sh/setup.system.motd.sh
@@ -4,4 +4,5 @@ sudo mv /etc/update-motd.d/* /etc/update-motd.d/backup/
 wget -O /etc/update-motd.d/01-altherneum 'https://raw.githubusercontent.com/Altherneum/server/main/etc/update-motd.d/01-altherneum'
 chmod +x /etc/update-motd.d/01-altherneum
 
-systemctl restart ssh
+echo "https://altherneum.fr" > /etc/motd
+echo "https://doc.altherneum.fr" >> /etc/motd
