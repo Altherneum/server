10f63a50b35c8c287e0d576868818f240fd32b7d
lx78WyY0J5 <9j2k37st@gmail.com>
2025-05-28 08:27:11 +0200

    set hostname

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+# Host name
+hostnamectl set-hostname altherneum.fr
+
 # Update + APT
 apt update
 apt install -y mysql-server phpmyadmin screen vsftpd openjdk-21-jdk curl wget unzip php php-fpm php-mysql php-pgsql php-sqlite3 php-bcmath php-mbstring php-xml php-curl php-zip php-gd apache2 sendmail fail2ban
