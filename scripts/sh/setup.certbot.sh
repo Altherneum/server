174640baf4dbeb27e50423290bc0c0a937458abc
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-05 12:22:59 +0200

    update to fix certbot issue

--- scripts/sh/setup.certbot.sh
+++ scripts/sh/setup.certbot.sh
@@ -8,4 +8,7 @@ ln -s /snap/bin/certbot /usr/bin/certbot
 certbot --apache certonly --agree-tos --register-unsafely-without-email -d altherneum.fr -d www.altherneum.fr -d play.altherneum.fr -d mc.altherneum.fr -d minecraft.altherneum.fr -d windows.altherneum.fr -d microsoft.altherneum.fr -d wiki.altherneum.fr -d info.altherneum.fr -d git.altherneum.fr -d github.altherneum.fr -d docs.altherneum.fr
 sudo certbot renew --dry-run
 #
+wget -O /etc/apache2/sites-available/001-le-ssl.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/apache2/sites-available/001-le-ssl.conf'
+a2ensite 001-le-ssl.conf
+#
 service apache2 start
