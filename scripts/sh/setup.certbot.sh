27e3e97ddaa2774a64327f9ce018da028e6bd866
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-05 08:14:39 +0200

    remove wildcard that need DNS challenges

--- scripts/sh/setup.certbot.sh
+++ scripts/sh/setup.certbot.sh
@@ -5,7 +5,7 @@ snap install --classic certbot
 #
 ln -s /snap/bin/certbot /usr/bin/certbot
 #
-certbot --apache certonly --agree-tos --register-unsafely-without-email -d altherneum.fr -d www.altherneum.fr -d play.altherneum.fr -d mc.altherneum.fr -d minecraft.altherneum.fr -d windows.altherneum.fr -d microsoft.altherneum.fr -d wiki.altherneum.fr -d info.altherneum.fr -d git.altherneum.fr -d github.altherneum.fr -d docs.altherneum.fr -d *.altherneum.fr
+certbot --apache certonly --agree-tos --register-unsafely-without-email -d altherneum.fr -d www.altherneum.fr -d play.altherneum.fr -d mc.altherneum.fr -d minecraft.altherneum.fr -d windows.altherneum.fr -d microsoft.altherneum.fr -d wiki.altherneum.fr -d info.altherneum.fr -d git.altherneum.fr -d github.altherneum.fr -d docs.altherneum.fr
 sudo certbot renew --dry-run
 #
 service apache2 start
