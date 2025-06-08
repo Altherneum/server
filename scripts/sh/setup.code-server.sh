cf87ea71d4c7eafaefbf07b46fcadf9ada65062e
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-08 04:23:14 +0200

    Update setup.code-server.sh

--- scripts/sh/setup.code-server.sh
+++ scripts/sh/setup.code-server.sh
@@ -2,6 +2,10 @@ VERSION=4.100.3
 curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_${VERSION}_amd64.deb
 sudo dpkg -i code-server_${VERSION}_amd64.deb
 
+# sudo ln -s ../sites-available/code-server /etc/apacge2/sites-available/code-server
+# or clone whats inside
+
+# add certbot domain code. to this one
 
 # User add "vscode" to avoid root passive SU power
 USER=vscode
