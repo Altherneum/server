225fa5b8061ee73530884dca54ccc18a6c51a4f6
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-08 04:26:00 +0200

    Update setup.system.user.sh

--- scripts/sh/setup.system.user.sh
+++ scripts/sh/setup.system.user.sh
@@ -4,6 +4,9 @@ useradd -r -g minecraft -d "/Serveurs" -s "/bin/bash" minecraft
 groupadd -r discord
 useradd -r -g discord -d "/DiscordBot" -s "/bin/bash" discord
 
+groupadd -r vscode
+useradd -r -g vscode -d "/home/vscode" -s "/bin/bash" --create-home vscode
+
 password="SuperPassword123!"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 groupadd -r admin
