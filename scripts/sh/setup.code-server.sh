362254e149e5ddec3da01b79dacec249c20cc97b
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-08 04:16:21 +0200

    Update setup.code-server.sh

--- scripts/sh/setup.code-server.sh
+++ scripts/sh/setup.code-server.sh
@@ -1,4 +1,8 @@
 VERSION=4.100.3
 curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_${VERSION}_amd64.deb
 sudo dpkg -i code-server_${VERSION}_amd64.deb
+
+
+# User add "vscode" to avoid root passive SU power
+USER=vscode
 sudo systemctl enable --now code-server@$USER
