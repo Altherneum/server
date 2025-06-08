a8c7336c7883822bb97406810e0bcaf78393bbef
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-08 04:15:28 +0200

    Create setup.code-server.sh

---
+++ scripts/sh/setup.code-server.sh
@@ -0,0 +1,4 @@
+VERSION=4.100.3
+curl -fOL https://github.com/coder/code-server/releases/download/v$VERSION/code-server_${VERSION}_amd64.deb
+sudo dpkg -i code-server_${VERSION}_amd64.deb
+sudo systemctl enable --now code-server@$USER
