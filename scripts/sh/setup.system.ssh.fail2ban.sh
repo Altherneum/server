afa3db0151066d3a2ed62413a8b649ca8eb27e9b
lx78WyY0J5 <9j2k37st@gmail.com>
2025-06-16 16:07:05 +0200

    update fail2ban config

--- scripts/sh/setup.system.ssh.fail2ban.sh
+++ scripts/sh/setup.system.ssh.fail2ban.sh
@@ -2,23 +2,9 @@
 systemctl start fail2ban
 systemctl enable fail2ban
 
-# Modification du fichier
-touch /etc/fail2ban/jail.d/jail.local
-
-echo "[sshd]" > /etc/fail2ban/jail.d/jail.local
-echo "enabled = true" >> /etc/fail2ban/jail.d/jail.local
-echo "port = 22" >> /etc/fail2ban/jail.d/jail.local
-
-echo "logpath = /var/log/auth.log" >> /etc/fail2ban/jail.d/jail.local
-
-echo "maxretry = 3" >> /etc/fail2ban/jail.d/jail.local
-echo "bantime = 24h" >> /etc/fail2ban/jail.d/jail.local
-echo "findtime = 10m" >> /etc/fail2ban/jail.d/jail.local
-echo "mode = aggressive" >> /etc/fail2ban/jail.d/jail.local
-
-echo "destemail = 9j2k37st@gmail.com" >> /etc/fail2ban/jail.d/jail.local
-echo "sender = fail2ban@altherneum.fr" >> /etc/fail2ban/jail.d/jail.local
-echo "mta = sendmail" >> /etc/fail2ban/jail.d/jail.local
+# Filtres
+wget -O /etc/fail2ban/jail.d/sshd.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/sshd.conf'
+wget -O /etc/fail2ban/jail.d/code-server.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/sshd.conf'
 
 # Relancer Fail2Ban pour appliquer les changements
 systemctl restart fail2ban
@@ -26,4 +12,6 @@ systemctl restart fail2ban
 # Vérification
 fail2ban-client status
 fail2ban-client -d
+# Vérification avec status
 fail2ban-client status sshd
+fail2ban-client status code-server
