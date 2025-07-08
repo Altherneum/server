b98b83626eab49a1dc96f9011cdeab0b786d9b74
lx78WyY0J5 <9j2k37st@gmail.com>
2025-07-08 17:05:52 +0200

    add code URL

--- scripts/sh/setup.system.motd.sh
+++ scripts/sh/setup.system.motd.sh
@@ -6,4 +6,5 @@ chmod +x /etc/update-motd.d/01-altherneum
 
 echo "https://altherneum.fr" > /etc/motd
 echo "https://doc.altherneum.fr" >> /etc/motd
+echo "https://code.altherneum.fr" >> /etc/motd
 echo "" >> /etc/motd
