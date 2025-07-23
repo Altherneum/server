48667c0ff940457e9fcbcad66db1bb63ebb96763
lx78WyY0J5 <9j2k37st@gmail.com>
2025-07-23 05:02:38 +0200

    comment to avoid breaking token inside

--- scripts/sh/update.runner.sh
+++ scripts/sh/update.runner.sh
@@ -25,7 +25,7 @@ chmod +x /scripts/sh/setup.system.user.sh
 wget -O /scripts/sh/setup.system.ssh.sh 'https://raw.githubusercontent.com/Altherneum/server/main/scripts/sh/setup.system.ssh.sh'
 chmod +x /scripts/sh/setup.system.ssh.sh
 # SSH WebHook
-wget -O /etc/ssh/sshrc 'https://raw.githubusercontent.com/Altherneum/server/main/etc/ssh/sshrc'
+# wget -O /etc/ssh/sshrc 'https://raw.githubusercontent.com/Altherneum/server/main/etc/ssh/sshrc'
 
 # Fail2Ban configuration
 wget -O /scripts/sh/setup.system.ssh.fail2ban.sh 'https://raw.githubusercontent.com/Altherneum/server/main/scripts/sh/setup.system.ssh.fail2ban.sh'
