395d1ee2042d4fc351588b2c18a65fd193fca40d
lx78WyY0J5 <9j2k37st@gmail.com>
2025-07-31 00:47:31 +0200

    update via version

--- Serveurs/DataFolder/update.sh
+++ Serveurs/DataFolder/update.sh
@@ -123,8 +123,8 @@ wget -O /Serveurs/Waterfall/waterfall.jar 'https://api.papermc.io/v2/projects/wa
 # Need to add folder & related shells & files
 # See config & diff (plugin change ???)
 
-wget -O /Serveurs/DataFolder/plugins/ViaBackwards.jar 'https://github.com/ViaVersion/ViaBackwards/releases/download/5.4.1/ViaBackwards-5.4.1.jar'
-wget -O /Serveurs/DataFolder/plugins/ViaVersion.jar 'https://github.com/ViaVersion/ViaVersion/releases/download/5.4.1/ViaVersion-5.4.1.jar'
+wget -O /Serveurs/DataFolder/plugins/ViaBackwards.jar 'https://github.com/ViaVersion/ViaBackwards/releases/download/5.4.2/ViaBackwards-5.4.2.jar'
+wget -O /Serveurs/DataFolder/plugins/ViaVersion.jar 'https://github.com/ViaVersion/ViaVersion/releases/download/5.4.2/ViaVersion-5.4.2.jar'
 wget -O /Serveurs/DataFolder/plugins/WorldEdit.jar 'https://mediafilez.forgecdn.net/files/4586/220/worldedit-bukkit-7.2.15.jar'
 # wget -O /Serveurs/DataFolder/plugins/WorldEdit.jar 'https://dev.bukkit.org/projects/worldedit/files/latest'
 
