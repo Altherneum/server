299e878c8e438befaedf7c85775168fbc04bbce3
lx78WyY0J5 <9j2k37st@gmail.com>
2025-07-31 00:37:07 +0200

    update ram usage

--- Serveurs/Hub/start.sh
+++ Serveurs/Hub/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/Hub/
-screen -dmS hub java -Xms128M -Xmx384M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Hub/paper.jar nogui
+screen -dmS hub java -Xms256M -Xmx384M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/Hub/paper.jar nogui
 echo screen hub
--- Serveurs/SkyBlock/start.sh
+++ Serveurs/SkyBlock/start.sh
@@ -1,3 +1,3 @@
 cd /Serveurs/SkyBlock/
-screen -dmS SkyBlock java -Xms128M -Xmx384M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/SkyBlock/paper.jar nogui
+screen -dmS SkyBlock java -Xms256M -Xmx384M -Dterminal.jline=false -XX:+ParallelRefProcEnabled -Dterminal.ansi=true -DIReallyKnowWhatIAmDoingISwear -Dcom.mojang.eula.agree=true -jar /Serveurs/SkyBlock/paper.jar nogui
 echo screen SkyBlock
