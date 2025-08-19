7825bd983a3a92792941f86abb511763391e80a3
lx78WyY0J5 <9j2k37st@gmail.com>
2025-08-19 18:05:18 +0200

    add another webhook on user login

---
+++ etc/profile.d/discord-webhook.sh
@@ -0,0 +1,21 @@
+
+ip="";
+username="@\`"$USER"\`"
+
+if [ -n "$SSH_CLIENT" ]; then
+    ip="\n  - IP \`"$(echo $SSH_CLIENT | cut -f 1 -d ' ')"\`";
+    text="📶 \`SSH\`";
+else
+    text="🖥️ \`Local\`";
+fi
+
+who="\n  - \`who -swu\` : \`"$(who -swu)"\`";
+pts="\n    - PTS \`"$(who -swu | cut -f 6 -d ' ')"\`";
+date="\n    - Depuis \`"$(who -swu | cut -f 14 -d ' ')"\`";
+hour="\`"$(who -swu | cut -f 15 -d ' ')"\`";
+
+curl --silent -v \
+-H "Content-Type: application/json" \
+-X POST \
+-d "{\"content\":\"- $text  $username $ip $who $pts $date\"}" \
+https://discord.com/api/webhooks/0000000000000000000/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa > /dev/null 2>&1
