9bca73ea2f63858b9feabad46c78c5c6421faa7b
lx78WyY0J5 <9j2k37st@gmail.com>
2025-09-05 20:53:56 +0200

    update certbot TLS for arch's domain

--- scripts/sh/setup.certbot.sh
+++ scripts/sh/setup.certbot.sh
@@ -5,7 +5,7 @@ snap install --classic certbot
 #
 ln -s /snap/bin/certbot /usr/bin/certbot
 #
-certbot --apache certonly --agree-tos --register-unsafely-without-email -d altherneum.fr -d www.altherneum.fr -d play.altherneum.fr -d mc.altherneum.fr -d code.altherneum.fr -d 3000.code.altherneum.fr -d vscode.altherneum.fr -d minecraft.altherneum.fr -d windows.altherneum.fr -d microsoft.altherneum.fr -d wiki.altherneum.fr -d info.altherneum.fr -d git.altherneum.fr -d github.altherneum.fr -d docs.altherneum.fr -d fuckmicrosoft.altherneum.fr -d fuckwindows.altherneum.fr
+certbot --apache certonly --agree-tos --register-unsafely-without-email -d altherneum.fr -d www.altherneum.fr -d play.altherneum.fr -d mc.altherneum.fr -d code.altherneum.fr -d 3000.code.altherneum.fr -d vscode.altherneum.fr -d arch.altherneum.fr -d archterneum.altherneum.fr -d git.arch.altherneum.fr -d minecraft.altherneum.fr -d windows.altherneum.fr -d microsoft.altherneum.fr -d wiki.altherneum.fr -d info.altherneum.fr -d git.altherneum.fr -d github.altherneum.fr -d docs.altherneum.fr -d fuckmicrosoft.altherneum.fr -d fuckwindows.altherneum.fr
 sudo certbot renew --dry-run
 #
 wget -O /etc/apache2/sites-available/001-le-ssl.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/apache2/sites-available/001-le-ssl.conf'
