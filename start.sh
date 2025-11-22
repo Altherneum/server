930641beb00561f79dd151c0869a75cd2a9d9ef9
lx78WyY0J5 <9j2k37st@gmail.com>
2025-11-22 05:30:59 +0100

    refactor auto start for MC servers

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
-# service minecraft start
-#
-# service minecraft@anarchie start
-# service minecraft@creatif start
-service minecraft@hub start
-# service minecraft@opprison start
-service minecraft@proxy start
-# service minecraft@rpg start
-service minecraft@skyblock start
-# service minecraft@survie start
+ /Serveurs/DataFolder/start-auto.sh
 #
 service discord start
 #
