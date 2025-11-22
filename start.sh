44c9e65278a8acefe4c35016b9182aafe6cff69d
lx78WyY0J5 <9j2k37st@gmail.com>
2025-11-22 05:06:59 +0100

    update service starter

--- start.sh
+++ start.sh
@@ -7,8 +7,13 @@ service minecraft@hub start
 service minecraft@proxy start
 # service minecraft@rpg start
 service minecraft@skyblock start
-service minecraft@survie start
+# service minecraft@survie start
 #
 service discord start
 #
 service code-server@vscode start
+#
+service fail2ban start
+#
+service sendmail start
+service opendkim start
