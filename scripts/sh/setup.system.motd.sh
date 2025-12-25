2bd983a18195b5a69c35b5ac6c5bd55471b39128
lx78WyY0J5 <9j2k37st@gmail.com>
2025-12-25 13:32:44 +0100

    update URLs

--- scripts/sh/setup.system.motd.sh
+++ scripts/sh/setup.system.motd.sh
@@ -6,5 +6,10 @@ chmod +x /etc/update-motd.d/01-altherneum
 
 echo "https://altherneum.fr" > /etc/motd
 echo "https://doc.altherneum.fr" >> /etc/motd
+echo "" >> /etc/motd
 echo "https://code.altherneum.fr" >> /etc/motd
+echo "https://keeweb.altherneum.fr" >> /etc/motd
+echo "https://git.altherneum.fr" >> /etc/motd
+echo "" >> /etc/motd
+echo "https://play.altherneum.fr" >> /etc/motd
 echo "" >> /etc/motd
