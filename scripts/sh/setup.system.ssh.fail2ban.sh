ce60402032e71dae97ccd9fc08b37e013e1af649
lx78WyY0J5 <9j2k37st@gmail.com>
2026-01-27 18:46:43 +0100

    ban 405 error to target bots

--- scripts/sh/setup.system.ssh.fail2ban.sh
+++ scripts/sh/setup.system.ssh.fail2ban.sh
@@ -9,12 +9,14 @@ wget -O /etc/fail2ban/jail.d/forgejo.conf 'https://raw.githubusercontent.com/Alt
 wget -O /etc/fail2ban/jail.d/keeweb.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/keeweb.conf'
 wget -O /etc/fail2ban/jail.d/404.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/404.conf'
 wget -O /etc/fail2ban/jail.d/env.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/env.conf'
+wget -O /etc/fail2ban/jail.d/405.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/405.conf'
 
 # Filter
 ## SSHD filter exist by default
 wget -O /etc/fail2ban/filter.d/apache2auth.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/filter.d/apache2auth.conf'
 wget -O /etc/fail2ban/filter.d/apache2error.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/filter.d/apache2error.conf'
 wget -O /etc/fail2ban/filter.d/apache2env.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/filter.d/apache2env.conf'
+wget -O /etc/fail2ban/filter.d/apache2notallowed.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/filter.d/apache2notallowed.conf'
 
 # Relancer Fail2Ban pour appliquer les changements
 systemctl restart fail2ban
