67a88a9f22b573fb2b1e07c5eb22249cb1d807b6
lx78WyY0J5 <9j2k37st@gmail.com>
2026-01-27 18:19:00 +0100

    update script to match new jail

--- scripts/sh/setup.system.ssh.fail2ban.sh
+++ scripts/sh/setup.system.ssh.fail2ban.sh
@@ -7,10 +7,12 @@ wget -O /etc/fail2ban/jail.d/sshd.conf 'https://raw.githubusercontent.com/Alther
 wget -O /etc/fail2ban/jail.d/code-server.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/code-server.conf'
 wget -O /etc/fail2ban/jail.d/forgejo.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/forgejo.conf'
 wget -O /etc/fail2ban/jail.d/keeweb.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/keeweb.conf'
+wget -O /etc/fail2ban/jail.d/keeweb.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/jail.d/404.conf'
 
 # Filter
 ## SSHD filter exist by default
 wget -O /etc/fail2ban/filter.d/apache2auth.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/filter.d/apache2auth.conf'
+wget -O /etc/fail2ban/filter.d/apache2auth.conf 'https://raw.githubusercontent.com/Altherneum/server/main/etc/fail2ban/filter.d/apache2error.conf'
 
 # Relancer Fail2Ban pour appliquer les changements
 systemctl restart fail2ban
@@ -20,4 +22,9 @@ fail2ban-client status
 fail2ban-client -d
 # Vérification avec status
 fail2ban-client status sshd
+
 fail2ban-client status code-server
+fail2ban-client status forgejo
+fail2ban-client status keeweb
+
+fail2ban-client status 404
