1d23e6b84e770e00dcc253574dbabf131545e7dc
lx78WyY0J5 <9j2k37st@gmail.com>
2026-04-15 02:32:23 +0200

    fix double folder

--- scripts/sh/backup-dump-folder.sh
+++ scripts/sh/backup-dump-folder.sh
@@ -9,7 +9,7 @@ mkdir -p $backupPATH/$folderNAME
 echo "Creating folder : " $backupPATH/$folderNAME
 
 # tar czf $backupPATH/$folderNAME.tar.gz $folderPATH/$folderNAME
-cp -rp $folderPATH/$folderNAME $backupPATH/$folderNAME
+cp -rp $folderPATH/$folderNAME $backupPATH
 
 echo "Copy folder : " $folderPATH/$folderNAME
 echo "To destination :" $backupPATH/$folderNAME
