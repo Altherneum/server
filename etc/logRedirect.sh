729f7c79098524be1e072399b1026a5b8dbe4a25
lx78WyY0J5 <9j2k37st@gmail.com>
2026-04-21 09:41:40 +0200

    Better discord webhook logging

--- etc/logRedirect.sh
+++ etc/logRedirect.sh
@@ -1,15 +1,16 @@
-#!bin/sh
-
 # Read each log line from apache piping out
 export IFS=
 while read -r line; do
 
-    payload=$(jq -nc --arg l "$line" '{"content": "- \($l)"}')
+    #payload=$line
+    payload=$(jq -nc --arg l "$line" '{"content": "\($l)"}')
 
-    curl --silent -v \
+     payload2=$(echo $payload | sed "s/LineFeed/\\\\n/g")
+
+    curl -v \
     -H "Content-Type: application/json" \
     -X POST \
-    -d "$payload" \
+    -d "$payload2" \
     https://discord.com/api/webhooks/0000000000000000000/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 done
