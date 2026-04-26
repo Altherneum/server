3ccf0783cb05408d53d510de3381b539c71d4afa
lx78WyY0J5 <9j2k37st@gmail.com>
2026-04-26 09:32:59 +0200

    update DNS

--- scripts/sh/setup.apache2-meme.sh
+++ scripts/sh/setup.apache2-meme.sh
@@ -1,6 +1,5 @@
 mkdir -p /var/www/meme/
 
-wget -O /var/www/meme/randomPage.html 'https://raw.githubusercontent.com/Altherneum/server/main/var/www/meme/randomPage.html'
+wget -O /var/www/meme/index.html 'https://raw.githubusercontent.com/Altherneum/server/main/var/www/meme/randomPage.html'
 
-chgrp www-data /var/www/meme/randomPage.html 
-chown www-data /var/www/meme/randomPage.html
+sudo chown -R www-data:www-data /var/www/meme/
