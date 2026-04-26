e1f49f2b9d51e6923b1a66f30b2d42c1abb8698a
lx78WyY0J5 <9j2k37st@gmail.com>
2026-04-26 09:07:16 +0200

    fix

--- scripts/sh/setup.apache2-meme.sh
+++ scripts/sh/setup.apache2-meme.sh
@@ -1,6 +1,6 @@
-wget -O /var/www/meme/randomPage.html 'https://raw.githubusercontent.com/Altherneum/server/main/var/www/meme/randomPage.html'
-
 mkdir -p /var/www/meme/
 
+wget -O /var/www/meme/randomPage.html 'https://raw.githubusercontent.com/Altherneum/server/main/var/www/meme/randomPage.html'
+
 chgrp www-data /var/www/meme/randomPage.html 
-chown www-data /var/www/meme/randomPage.htmls
+chown www-data /var/www/meme/randomPage.html
